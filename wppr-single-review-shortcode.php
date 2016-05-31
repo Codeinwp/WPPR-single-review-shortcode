defd4c6a523c609f583f79ff66ecf349731bab60
contactashish13 <contactashish13@gmail.com>
2016-05-31 10:12:56 +0530

    tgm for free plugin #8

--- wppr-single-review-shortcode.php
+++ wppr-single-review-shortcode.php
@@ -10,6 +10,7 @@
 #	  Domain Path: /languages
 #     */  
 
+define("WPPR_SRS_PATH", realpath(dirname(__FILE__)));
 
 function wppr_srs_cwp_get_rating( $atts ) {
 		$a = shortcode_atts( array(
@@ -37,3 +38,11 @@ function wppr_srs_cwp_get_rating( $atts ) {
 if (!shortcode_exists('P_REVIEW' )) {
 	add_shortcode( 'P_REVIEW', 'wppr_srs_cwp_get_rating');
 }
+
+
+// Added by Ash/Upwork
+function wppr_srs_load_dependencies(){
+    require_once WPPR_SRS_PATH . "/lib/dependencies/tgm-activation.php";
+}
+add_action('plugins_loaded', 'wppr_srs_load_dependencies');
+// Added by Ash/Upwork
